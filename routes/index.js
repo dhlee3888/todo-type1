688ab8e8786564909d815ce6e94f486506334dc6
simon <dhlee3888@gmail.com>
2019-04-23 18:59:29 +0900

    일단 실행되게 수정함

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var todo = require('./todo.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '간단한 ToDo 리스트 예제 실습' });
 });
+
+router.get('/list', todo.list);
+router.post('/add', todo.add);
+router.post('/complete', todo.complete);
+router.post('/del', todo.del);
 
 module.exports = router;
